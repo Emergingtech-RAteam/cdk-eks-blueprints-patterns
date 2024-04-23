c6c14d10280cb0f5f3553db26a8cc0674c53e0b3
deloittepocra <deloittepocra@gmail.com>
2024-04-23 04:38:05 -0400

    added const gitOwner

--- lib/pipeline-multi-env-gitops/index.ts
+++ lib/pipeline-multi-env-gitops/index.ts
@@ -101,6 +101,7 @@ export default class PipelineMultiEnvGitops {
 
         try {
             // TODO - add dynamic gitowner suport when using codeStar config const { gitOwner, gitRepositoryName } = await getRepositoryData();
+            const gitOwner = 'deloittepocra@gmail.com'
             const gitRepositoryName = 'cdk-eks-blueprints-patterns';
 
             blueprints.CodePipelineStack.builder()
